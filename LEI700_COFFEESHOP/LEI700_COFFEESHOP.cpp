9ccba164b1c070982b2f05c0b1e2ff3d98da1563
Raman <30530716+ultradojd@users.noreply.github.com>
2020-09-13 16:17:11 +0300

    Update LEI700_COFFEESHOP.cpp

--- LEI700_COFFEESHOP/LEI700_COFFEESHOP.cpp
+++ LEI700_COFFEESHOP/LEI700_COFFEESHOP.cpp
@@ -1,20 +1,117 @@
-﻿// LEI700_COFFEESHOP.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+ /*
+ COFFEESHOP. Компания EspressoBiancci заказывает разработку системы управления меню для своего кофейного аппарата LEI700. 
+Аппарат готовит кофе трех видов: Espresso, Cappuccino, Latte
+После ввода PIN:
+* Просмотр баланса
+* Просмотр кол-ва оставшихся пустых стаканчиков
+* Изъятие выручки 
+Примечания:
+* Программно-аппаратный комплекс LEI700 поддерживает языки: Assembler, C, C++.     
+* PIN - специальный код из 4 цифр, предоставляемый EspressoBiancci представителям сервиса под роспись.
+ 
+1. Разделитесь по парам
+2. Договоритесь с напарником о времени митинга в скайпе  (1-2 раза в неделю) и явно запишите его
+2. Работая в паре, обсудите проект. Помните, что хороший код - надежный, продумайте, какие исключительные ситуации могут возникнуть при заказе кофе и работе сотрудников сервиса
+3. Критически важно на этом этапе задавать вопросы, иначе, по статистике, у вас есть 75% сделать работающий проект и услышать знаменитую фразу заказчика: "А я хотел не это"
+4. Разработайте UI системы (точный вид меню)
+5. Утвердите UI у заказчика
+6. Договоритесь, кто какие функции пишет в проекте
+7. Начинайте разработку. Используйте метод пошаговой разработки, начните с меню (пока функции, вызываемые из меню, будут заглушками), т.е. вы сделаете прототип системы
+8. Утвердите прототип у своего ментора
+9. Теперь постепенно наращивайте функциональность своего проекта, сопровождая этот процесс тестированием
+10. Проведите crash-test системы, особенно чатей, написанных напарников (чужие ошибки ищутся лучше). Подготовьтесь к DEMO проекта.
+ */
 
 #include <iostream>
+using namespace std;
+
+#define PIN 1234
+
+void printMenu();
+void cookEspresso();
+void cookCappuccino();
+void cookLatte();
+void openServiceMenu(); 
+void autentification();
+int amtSugar();
+int numbOfSpoons();
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int choice = 0;
+    double balance = 100.0;
+    int cups = 50;
+    int amountCoffee = 1000; //gramme
+    int amountMilk = 1000;  //milliliters
+    int amountWater = 10000;    //milliliters
+    int amountSugar = 1000; //gramme
+
+    cout << "Hello! Please select coffee." << endl;
+
+    while (true) {
+        printMenu();
+        cin >> choice;
+        if (choice == 1)
+            cookEspresso();
+        else if (choice == 2)
+            cookCappuccino();
+        else if (choice == 3)
+            cookLatte();
+        else if (choice == 4)
+            autentification();
+    }
+    return 0;
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+void printMenu() {
+    cout << endl << "1. Espresso" << endl;
+    cout << "2. Cappuccino" << endl;
+    cout << "3. Latte" << endl;
+    cout << "4. Service" << endl;
+    cout << "Your choice? ";
+}
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+void cookEspresso() {
+    cout << "Espresso is ready." << endl;
+}
+
+void cookCappuccino() {
+    cout << "Cappuccino is ready" << endl;
+}
+
+void cookLatte() {
+    cout << "Cappuccino is ready" << endl;
+}
+
+void openServiceMenu() {
+    cout << "Service is open." << endl;
+}
+
+void autentification() {
+    int pin = 0;
+    cout << "Please, enter your PIN: ";
+    cin >> pin;
+    if (pin == PIN)
+        openServiceMenu();
+    else
+        cout << "Your PIN is not valid." << endl;
+}
+
+int amtSugar() {
+    int sugar = 0;
+    cout << "Do you want to add sugar? ";
+    cin >> sugar;
+    if (sugar > 0)
+        numbOfSpoons();
+    return 0;
+}
+
+int numbOfSpoons() {
+    int spoons = 0;
+    cout << "How much spoons of sugar do you want? ";
+    cin >> spoons;
+    if (spoons == 1)
+        cout << "One spoon of sugar.";
+    else if (spoons == 2)
+        cout << "Two spoons of sugar.";
+}
